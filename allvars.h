f97dcd9a49f091acc804a501974b98d4ad98bcf3
NATYML <nataly@gmail.com>
2017-02-09 19:29:07 -0500

    TSC

--- allvars.h
+++ allvars.h
@@ -92,4 +92,5 @@ struct gadget_header{
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_statistics.h>
 #include <time.h>
+
 //#include "hdf5.h"
--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ make
 #./main.out ../Box_50x256_100 ./params.dat
 #./main.out ./pos.dat ./ascci_params.
 #./main.out ../Box400/Box_400_512_150 ./params.dat
-./main.out ../MDPL_halos_data/MDPL_Haloes_10e11.csv ./ascci_params.dat
+./main.out ../MDPL_halos_data/MDPL_Haloes_10e13.csv ./ascci_params.dat
