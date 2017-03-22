b2521f914538bd65ab424cb2648a5ca5fc4f31c7
natyml <natyml20@gmail.com>
2017-03-22 15:13:48 -0500

    update

--- Fitting_code/script.sh
+++ Fitting_code/script.sh
@@ -1,5 +1,7 @@
-rm ./pk_fit/pk*
+#rm ./pk_fit/pk*
 make clean
 make
 #./fit.out ../../output_files/PS_TSC_1e11.dat > pk
-./fit.out ./PS_TSC_1e11.dat 
+#./fit.out ./PS_TSC_1e11.dat 
+./fit.out  ./pk/PS_0.50_kf.dat
+
